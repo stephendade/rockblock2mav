0a767cf51a958ae9d832ab09c769e45c9b2d0fe2
Stephen Dade <stephen_dade@hotmail.com>
2025-02-11 19:59:07 +1100

    Add mavlink2 support

--- rockblock2mav-adafruit.py
+++ rockblock2mav-adafruit.py
@@ -19,15 +19,13 @@ mavproxy.py --master=udpout:127.0.0.1:16000 --streamrate=1 --console --mav10 --m
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
 import json
-import socket
 import sys
 import time
 import requests
-import errno
 from urllib.parse import quote
 
 from Adafruit_IO import Client, errors
-from pymavlink.dialects.v10 import ardupilotmega as mavlink1
+import pymavlink.mavutil as mavutil
 
 ROCK7_URL = 'https://rockblock.rock7.com/rockblock/MT'
 
@@ -69,30 +67,24 @@ if __name__ == '__main__':
     parser.add_argument("-adafruitfeed", help="Adafruit.io feed name")
     parser.add_argument("-adafruitkey", help="Adafruit.io key")
     parser.add_argument("-imei", help="Iridium Modem IMEI")
-    parser.add_argument("-out", default="udpin:127.0.0.1:16000", help="MAVLink udpin:IP:Port or udpout:IP:Port to output packets to")
+    parser.add_argument("-out", default="udpin:127.0.0.1:16000", help="MAVLink connection to GCS")
+    parser.add_argument("-debug", default="udpin:127.0.0.1:17000", help="Debugging port to view messages sent from GCS to vehicle")
     parser.add_argument("-rock7username", help="Rock7 username")
     parser.add_argument("-rock7password", help="Rock7 password")
+    parser.add_argument("-mav20", action='store_true', default=False, help="Use MAVLink 2.0 on -out")
 
     args = parser.parse_args()
 
     aio = Client(args.adafruitusername, args.adafruitkey)
     lastpacket = None
 
-    mavUAV = mavlink1.MAVLink(255, 0, use_native=False)
-    mavGCS = mavlink1.MAVLink(255, 0, use_native=False)
-
-    udp_dir = args.out.split(':')[0]
-    out_ip = args.out.split(':')[1]
-    out_port = int(args.out.split(':')[2])
-
-    clientIPPort = None
-
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    UDPClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    if udp_dir == 'udpin':
-        UDPClientSocket.bind((out_ip, out_port))
-    UDPClientSocket.settimeout(1.0)
-    UDPClientSocket.setblocking(0)
+    mavGCS = mavutil.mavlink_connection(args.out)  # Sends packets vehicle -> GCS
+    mavUAV = mavutil.mavlink_connection(args.debug)   # Repacks packets GCS -> vehicle
+    mavUAV.WIRE_PROTOCOL_VERSION = "1.0"
+    if args.mav20:
+        mavGCS.WIRE_PROTOCOL_VERSION = "2.0"
+    else:
+        mavGCS.WIRE_PROTOCOL_VERSION = "1.0"
 
     while True:
         raw_data = None
@@ -130,69 +122,66 @@ if __name__ == '__main__':
             if msgList:
                 for msg in msgList:
                     print(msg)
+                    mavGCS.mav.srcSystem = msg.get_srcSystem()
+                    mavGCS.mav.srcComponent = msg.get_srcComponent()
+                    mavGCS.mav.seq = msg.get_seq()
+                    if args.mav20:
+                        mavGCS.mav.send(msg, force_mavlink1=False)
+                    else:
+                        mavGCS.mav.send(msg, force_mavlink1=True)
 
-            # send on to GCS (raw bytes)
-            if clientIPPort and udp_dir == 'udpin':
-                UDPClientSocket.sendto(bytes.fromhex(data['data']), clientIPPort)
-            elif udp_dir == 'udpout':
-                UDPClientSocket.sendto(bytes.fromhex(data['data']), (out_ip, out_port))
         elif lastpacket != data:
             print("Adafruit.io packet too old. Packet time = {0}, Current time = {1}".format(datetime_object, datetime.utcnow()))
 
-        # get incoming bytes from GCS
-        data = None
-        addr = None
+        # get incoming bytes from GCS to send to vehicle
         do_check = True
+        all_msgbuf = ''
         while do_check:
             print("Checking for GCS packets")
-            try:
-                data, addr = UDPClientSocket.recvfrom(UDP_MAX_PACKET_LEN)
-            except socket.error as e:
-                if e.errno in [errno.EAGAIN, errno.EWOULDBLOCK, errno.ECONNREFUSED]:
-                    do_check = False
+            msgGCS = mavGCS.recv_msg()
+            # filter according to msg properties and send buffer to Rock7
+            if msgGCS:
+                # convert to mavlink1 if needed. Get buffer of hex bytes too
+                msgbuf = None
+                if args.mav20:
+                    mavUAV.mav.srcSystem = msgGCS.get_srcSystem()
+                    mavUAV.mav.srcComponent = msgGCS.get_srcComponent()
+                    mavUAV.mav.seq = msgGCS.get_seq()
+                    # repack in MAVLink1 format
+                    msgbuf = msgGCS.pack(mavUAV.mav, force_mavlink1=True)
                 else:
-                    raise
-            if data:
-                clientIPPort = addr
-                try:
-                    msgList = mavGCS.parse_buffer(data)
-                except mavlink1.MAVError:
-                    pass
-                if msgList:
-                    # queue up the messages (up to 50 bytes. Note format is 00 for each byte, so doubled) for sending 
-                    # to the RockBlock
-                    all_msgbuf = ''
-                    for msg in msgList:
-                        # Filter by acceptable messages and commands
-                        if msg.get_type() in ['COMMAND_LONG', 'COMMAND_INT'] and int(msg.command) in ALLOWABLE_CMDS and len(all_msgbuf) <= 50:
-                            print("Adding to send queue: " + str(msg))
-                            all_msgbuf += "".join("%02x" % b for b in msg.get_msgbuf())
-                            print("Message buffer length: {0}/50".format(len(all_msgbuf)/2))
-                        elif msg.get_type() in ALLOWABLE_MESSAGES and len(all_msgbuf) <= 50:
-                            all_msgbuf += "".join("%02x" % b for b in msg.get_msgbuf())
-                            print("Adding to send queue: " + str(msg))
-                            print("Message buffer length: {0}/50".format(len(all_msgbuf)/2))
-                        elif len(all_msgbuf) > 50:
-                            print("Message buffer full, not adding {0}".format(msg.get_type()))
-                    if all_msgbuf:
-                        url = "{0}?imei={1}&username={2}&password={3}&data={4}&flush=yes".format(ROCK7_URL,
-                                                                                                 args.imei,
-                                                                                                 quote(args.rock7username),
-                                                                                                 quote(args.rock7password),
-                                                                                                 all_msgbuf)
-                        response = requests.post(url, headers={"Accept": "text/plain"})
-                        responseSplit = response.text.split(',')
-                        if len(all_msgbuf)/2 > 50:
-                            print("Warning, messages greater than 50 bytes")
-                        if responseSplit[0] != 'OK' and len(responseSplit) > 1:
-                            if responseSplit[1] in ROCK7_TX_ERRORS.keys():
-                                print("Error sending command: " + ROCK7_TX_ERRORS[responseSplit[1]])
-                            else:
-                                print("Unknown error: " + response)
-                        else:
-                            print("Sent {0} bytes OK".format(len(msg.get_msgbuf())))
+                    msgbuf = msgGCS.get_msgbuf()
+                # Filter by acceptable messages and commands
+                if msgGCS.get_type() in ['COMMAND_LONG', 'COMMAND_INT'] and int(msgGCS.command) in ALLOWABLE_CMDS and len(all_msgbuf) <= 50:
+                    print("Adding to send queue: " + str(msgGCS))
+                    all_msgbuf += "".join("%02x" % b for b in msgbuf)
+                    print("Message buffer length: {0}/50".format(len(all_msgbuf)/2))
+                elif msgGCS.get_type() in ALLOWABLE_MESSAGES and len(all_msgbuf) <= 50:
+                    all_msgbuf += "".join("%02x" % b for b in msgbuf)
+                    print("Adding to send queue: " + str(msgGCS))
+                    print("Message buffer length: {0}/50".format(len(all_msgbuf)/2))
             else:
                 # We've gotten all bytes from the GCS
                 do_check = False
+        # send bytes to Rockblock, if any
+        if all_msgbuf:
+            print(all_msgbuf)
+            url = "{0}?imei={1}&username={2}&password={3}&data={4}&flush=yes".format(ROCK7_URL,
+                                                                                     args.imei,
+                                                                                     quote(args.rock7username),
+                                                                                     quote(args.rock7password),
+                                                                                     all_msgbuf)
+            response = requests.post(url, headers={"Accept": "text/plain"})
+            responseSplit = response.text.split(',')
+            if len(all_msgbuf)/2 > 50:
+                print("Warning, messages greater than 50 bytes")
+            if responseSplit[0] != 'OK' and len(responseSplit) > 1:
+                if responseSplit[1] in ROCK7_TX_ERRORS.keys():
+                    print("Error sending command: " + ROCK7_TX_ERRORS[responseSplit[1]])
+                else:
+                    print("Unknown error: " + response)
+            else:
+                print("Sent {0} bytes OK".format(len(all_msgbuf)/2))
+            all_msgbuf = ''
         print("Sleeping 2")
         time.sleep(2)
